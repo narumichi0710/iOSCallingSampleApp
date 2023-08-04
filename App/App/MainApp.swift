89c3a1a3e54b53c168b62c99fc06bc41ccc02609
Narumichi Kubo <narumichi7373@gmail.com>
2023-08-04 16:54:44 +0900

    init

---
+++ App/App/MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppApp.swift
+//  App
+//
+//  Created by Narumichi Kubo on 2023/08/04.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
