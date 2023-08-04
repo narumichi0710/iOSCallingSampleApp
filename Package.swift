58745efd04262df2825cfd230a3d007ce26f874d
Narumichi Kubo <narumichi7373@gmail.com>
2023-08-04 17:08:12 +0900

    ターゲットバージョンを変更

--- Package.swift
+++ Package.swift
@@ -5,15 +5,13 @@ import PackageDescription
 
 let package = Package(
     name: "iOSCallingSampleApp",
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "iOSCallingSampleApp",
             targets: ["iOSCallingSampleApp"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "iOSCallingSampleApp"),
         .testTarget(
